81f51c2b7d514677bd8284ce84f010f222c0b60e
muratanrisever <murat_tanrisever2006@hotmail.com>
2025-08-07 14:41:13 +0300

    Second commit.

---
+++ SiparisEntegrasyonu/Module1.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports Newtonsoft.Json
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Sipariş entegrasyonu başlatıldı...")
+
+        Dim client As New HttpClient()
+        client.BaseAddress = New Uri("https://localhost:7283/") ' API adresi
+        client.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+
+        Dim response = client.GetAsync("api/tumSiparisler").Result
+
+        If response.IsSuccessStatusCode Then
+            Dim json = response.Content.ReadAsStringAsync().Result
+            Dim siparisler As List(Of SiparisDto) = JsonConvert.DeserializeObject(Of List(Of SiparisDto))(json)
+
+            For Each siparis In siparisler
+                If Not SiparisVarMi(siparis.Id) Then
+                    SiparisEkle(siparis)
+                    Console.WriteLine($"Yeni sipariş eklendi: {siparis.AdiSoyadi}")
+                End If
+            Next
+        Else
+            Console.WriteLine("API'den sipariş çekilemedi.")
+        End If
+
+        Console.WriteLine("İşlem tamamlandı.")
+    End Sub
+
+    Function SiparisVarMi(id As Integer) As Boolean
+        Dim cs = ConfigurationManager.ConnectionStrings("MyDbConnection").ConnectionString
+        Using conn As New SqlConnection(cs)
+            conn.Open()
+            Dim cmd As New SqlCommand("SELECT COUNT(*) FROM Siparisler WHERE Id = @id", conn)
+            cmd.Parameters.AddWithValue("@id", id)
+            Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+            Return count > 0
+        End Using
+    End Function
+
+    Sub SiparisEkle(siparis As SiparisDto)
+        Dim cs = ConfigurationManager.ConnectionStrings("MyDbConnection").ConnectionString
+        Using conn As New SqlConnection(cs)
+            conn.Open()
+            Dim cmd As New SqlCommand("INSERT INTO Siparisler (Id, AdiSoyadi, Urun, Adet, Tarih) VALUES (@id, @adi, @urun, @adet, @tarih)", conn)
+            cmd.Parameters.AddWithValue("@id", siparis.Id)
+            cmd.Parameters.AddWithValue("@adi", siparis.AdiSoyadi)
+            cmd.Parameters.AddWithValue("@urun", siparis.Urun)
+            cmd.Parameters.AddWithValue("@adet", siparis.Adet)
+            'cmd.Parameters.AddWithValue("@tarih", siparis.Tarih)
+            cmd.ExecuteNonQuery()
+        End Using
+    End Sub
+
+    Public Class SiparisDto
+        Public Property Id As Integer
+        Public Property AdiSoyadi As String
+        Public Property Urun As String
+        Public Property Adet As Integer
+        Public Property Tarih As DateTime
+    End Class
+
+End Module
---
+++ SiparisEntegrasyonu/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SiparisEntegrasyonu")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SiparisEntegrasyonu")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2ec16880-9a1c-441d-aa34-2c22d52f8fd7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
